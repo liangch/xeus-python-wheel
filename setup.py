d55bc34b3a186810b963a33bb31e8732b6f09af0
Liang Chen <175010+liangch@users.noreply.github.com>
2025-02-13 12:29:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 import os
 import sys
 
-try:
-    from skbuild import setup
-    from skbuild.exceptions import SKBuildError
-    from skbuild.cmaker import get_cmake_version
-    from packaging.version import LegacyVersion
+setup_requires = []
 
-    setup_requires = []
-    try:
-        if LegacyVersion(get_cmake_version()) < LegacyVersion("3.15"):
-            setup_requires.append('cmake')
-    except SKBuildError:
-        setup_requires.append('cmake')
-except ImportError:
-    print('scikit-build is required to build from source.', file=sys.stderr)
-    print('Please run:', file=sys.stderr)
-    print('', file=sys.stderr)
-    print('  python -m pip install scikit-build')
-    sys.exit(1)
+# try:
+#     from skbuild import setup
+#     from skbuild.exceptions import SKBuildError
+#     from skbuild.cmaker import get_cmake_version
+#     from packaging.version import LegacyVersion
+
+#     setup_requires = []
+#     try:
+#         if LegacyVersion(get_cmake_version()) < LegacyVersion("3.15"):
+#             setup_requires.append('cmake')
+#     except SKBuildError:
+#         setup_requires.append('cmake')
+# except ImportError:
+#     print('scikit-build is required to build from source.', file=sys.stderr)
+#     print('Please run:', file=sys.stderr)
+#     print('', file=sys.stderr)
+#     print('  python -m pip install scikit-build')
+#     sys.exit(1)
 
 python_path = sys.executable
 
